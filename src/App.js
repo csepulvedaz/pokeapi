a0105d569a1f8e511c73dc6c52d2437ab7c14202
Camilo J Sepulveda <csepulveda@smarthop.co>
2023-02-15 19:53:03 -0500

    Installing dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useTranslation } from "react-i18next";
+
+const LANGUAGES = {
+	es: { nativeName: "Español" },
+	en: { nativeName: "English" },
+};
+
 function App() {
-	return <div>Init</div>;
+	const { t, i18n } = useTranslation();
+
+	return (
+		<div>
+			<div>
+				{Object.keys(LANGUAGES).map((lng) => (
+					<button key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>
+						{LANGUAGES[lng].nativeName}
+					</button>
+				))}
+			</div>
+			<p>{t("initialization")}</p>
+		</div>
+	);
 }
 
 export default App;
